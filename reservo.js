5a375df02c0529e6f76902e2e812535e97472035
Thomas <me@thomassnielsen.com>
2013-03-25 18:45:34 +0100

    Lagt inn alle input felt (tidsvelger ikke aktiv enda)
    Signed-off-by: Thomas <me@thomassnielsen.com>

--- reservo.js
+++ reservo.js
@@ -8,22 +8,106 @@ function loadRestaurant(id)
 	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
 	    {
 		    parseRestaurantJSON(xmlhttp.responseText);
+		    createInputFields();
 	    }
 	  }
 	xmlhttp.open("POST","http://pido.cc/api_open/get_restaurant_by_id",true);
 	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	xmlhttp.send("id="+id);
-	
+}
+
+function createInputFields()
+{
 	var inputSection = document.createElement("section");
 	inputSection.id = "reservoInput";
 	
-	var datePicker = document.createElement("Input");
-	datePicker.type = "date";
+	// Example: <input type="text" placeholder="Jan Petter">
+	var nameField = document.createElement("input");
+	nameField.type = "text";
+	nameField.setAttribute("placeholder", "Jan Petter");
 	
+	// Example: <input type="number" max="24" min="1" value="3">
+	var peopleCountField = document.createElement("input");
+	peopleCountField.type = "number";
+	peopleCountField.setAttribute("max", reservoRestaurantData.max_booking);
+	peopleCountField.setAttribute("min", reservoRestaurantData.min_booking);
+	peopleCountField.setAttribute("value", "4");
+	
+	// 
+	var weekday=new Array(7);
+	weekday[0]="søndag";
+	weekday[1]="mandag";
+	weekday[2]="tirsdag";
+	weekday[3]="onsdag";
+	weekday[4]="torsdag";
+	weekday[5]="fredag";
+	weekday[6]="lørdag";
+	
+	var date = new Date();
+	var today = date.getDay();
+	
+	var datePicker = document.createElement("select");
+	for (i = 0; i < 14; i++)
+	{
+		var thisDay = today+i;
+		
+		while (thisDay > 6)
+			thisDay -= 7;
+			
+		var option = document.createElement("option");
+		option.value = i;
+		if (i > 6)
+			option.innerHTML = "Neste ";
+		option.innerHTML += weekday[thisDay];
+		
+		if (i == 0)
+			option.innerHTML = "i dag";
+		if (i == 1)
+			option.innerHTML = "i morgen";
+		
+		datePicker.appendChild(option);
+	}
+	
+	var timePicker = document.createElement("select");
+	// for (time in availabletimes)
+	for (i = 0; i < 24*4; i++)
+	{
+		var time = i*15;
+		var hour = parseInt(time/60);
+		var minute = time-hour*60;
+		
+		var option = document.createElement("option");
+		option.value = i;
+		if (minute < 10)
+			minute = '0'+minute;
+		if (hour < 10)
+			hour = '0'+hour;
+		option.innerHTML = hour + ':' + minute;
+		
+		if (i == 24*3)
+			option.setAttribute("selected", "selected");
+		
+		timePicker.appendChild(option);
+	}
+	
+	var phoneField = document.createElement("input");
+	phoneField.type = "phone";
+	phoneField.setAttribute("placeholder", "12345678");
+	
+	// We need a montage!
+	inputSection.innerHTML += "Jeg, ";
+	inputSection.appendChild(nameField);
+	inputSection.innerHTML += " ønsker å reservere et bord for ";
+	inputSection.appendChild(peopleCountField);
+	inputSection.innerHTML += " personer på "+reservoRestaurantData.name+" ";
 	inputSection.appendChild(datePicker);
+	inputSection.innerHTML += " klokken ";
+	inputSection.appendChild(timePicker);
+	inputSection.innerHTML += ". Dere kan nå meg på ";
+	inputSection.appendChild(phoneField);
+	inputSection.innerHTML += " (mobil).";
 	
 	document.body.appendChild(inputSection);
-	
 }
 
 function parseRestaurantJSON(data)
@@ -31,7 +115,7 @@ function parseRestaurantJSON(data)
 	var parsedData = JSON.parse(data);
 	reservoRestaurantData = parsedData;
 	
-	addStationElementsToDOM();
+//	addStationElementsToDOM();
 }
 
 function addStationElementsToDOM()
